0d3e799e4c1b085f147046b82ed8d92304ae38f1
AngeD <ange.duhayon@epitech.eu>
2022-02-15 23:40:49 +0100

    feat: beginning of game window in pygame

--- freecell/freecell.py
+++ freecell/freecell.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python3
+import pygame as pg
 import random
 
-numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
-colors = ['H', 'C', 'D', 'S']
+ASSETS = [
+    "../assets/cards.png"
+]
+
+NUMBERS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+COLORS = ['H', 'C', 'D', 'S']
 
 class Card:
-    def __init__(self, number: str, col: str):
-        if not number in numbers or not col in colors:
-            return None
-        self.value = number + col
+    def __init__(self, surface: pg.surface, number: str, color: str):
+        try:
+            self.value = number + color
+            self.surface = surface
+            self.rect = pg.Rect(
+                NUMBERS.index(number) * 81,
+                COLORS.index(color) * 117,
+                81,
+                117
+            )
+        except Exception as e:
+            print(f"{number + color}\n{e}")
+            exit(1)
 
-def initCards(seed = None, shuffle = True) -> list[Card]:
+def initCards(surface: pg.Surface, seed = None, shuffle = True) -> list[Card]:
     deck = [[] for i in range(8)]
 
     if not seed:
-        cards = [Card(nb, col) for col in colors for nb in numbers]
+        cards = [Card(surface, nb, color) for color in COLORS for nb in NUMBERS]
         if shuffle:
             random.shuffle(cards)
     else:
-        cards = [Card(card[0], card[1]) for card in seed.split(" ")]
+        cards = [Card(sprite, card[0], card[1]) for card in seed.split(" ")]
     for col in deck[:4]:
         for i in range(7):
             col.append(cards.pop())
@@ -27,11 +41,24 @@ def initCards(seed = None, shuffle = True) -> list[Card]:
             col.append(cards.pop())
     return deck
 
-def main():
-    currentCards = initCards(shuffle = True)
+def checkEvents():
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
 
-    for col in currentCards:
-        print(f"[{' '.join(card.value for card in col)}]")
+def main():
+    window = pg.display.set_mode((800, 600), flags = pg.RESIZABLE)
+    cardsIMG = pg.image.load(ASSETS[0])
+    cards = initCards(cardsIMG)
+
+    while 1:
+        checkEvents()
+        for i, col in enumerate(cards):
+            for j, card in enumerate(col):
+                window.blit(card.surface, (i * 91, j * 117 / 2), card.rect)
+        pg.display.flip()
 
 if __name__ == "__main__":
+    pg.init()
     main()
+    pg.quit()
