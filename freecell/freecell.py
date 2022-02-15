c1e6da8d3fc20ff98997f4e9b21bef0a61b9d4e2
AngeD <ange.duhayon@epitech.eu>
2022-02-15 18:34:19 +0100

    feat: seed

--- freecell/freecell.py
+++ freecell/freecell.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 import random
 
-numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
-colors = ['H', 'C', 'D', 'S'];
+numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+colors = ['H', 'C', 'D', 'S']
 
 class Card:
     def __init__(self, number: str, col: str):
         if not number in numbers or not col in colors:
-            return none
+            return None
         self.value = number + col
 
-def initCards() -> list[Card]:
-    cards = [Card(nb, col) for col in colors for nb in numbers]
+def initCards(seed = None, shuffle = True) -> list[Card]:
     deck = [[] for i in range(8)]
 
-    random.shuffle(cards)
+    if not seed:
+        cards = [Card(nb, col) for col in colors for nb in numbers]
+        if shuffle:
+            random.shuffle(cards)
+    else:
+        cards = [Card(card[0], card[1]) for card in seed.split(" ")]
     for col in deck[:4]:
         for i in range(7):
             col.append(cards.pop())
@@ -24,10 +28,10 @@ def initCards() -> list[Card]:
     return deck
 
 def main():
-    currentCards = initCards()
+    currentCards = initCards(shuffle = True)
+
     for col in currentCards:
-        for card in col:
-            print(card.value)
+        print(f"[{' '.join(card.value for card in col)}]")
 
 if __name__ == "__main__":
     main()
